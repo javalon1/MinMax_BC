c77efdd17d266a03315bf9ab90cacd4a46d21787
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-12 13:09:06 +0100

    first commit

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -6,8 +6,9 @@ package com.sda.algorytmy;
  */
 public class App 
 {
-    public static void main( String[] args )
-            
+    public static void main( String[] args ){
+
+
     {
         System.out.println( "Hello World!" );
     }
