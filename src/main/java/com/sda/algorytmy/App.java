73c65dcb3653caf0679eb6b2526ad815effaeacf
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-12 14:04:12 +0100

    done

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,14 +2,25 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ){
+public class App {
+    public static void main(String[] args) {
 
+        int[] a = {4, 6, 2, 1, 3};
+        int wynik = a[0];
+        int maximum = a[0];
 
-    {
-        System.out.println( "Hello World!" );
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] > maximum) {
+                maximum = a[i];
+            }
+
+            if (a[i] < wynik) {
+                wynik = a[i];
+            }
+        }
+            System.out.println("element minimalny:" + wynik);
+            System.out.println("element maksymalny:" + maximum);
+        }
     }
-}
